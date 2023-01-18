a5a3097c2f1368f78aec0d5cabcff0e9a7fe2055
Karthik Menon <karthikmenon.13@gmail.com>
2023-01-17 19:15:29 -0800

    abstract class for svZeroDModel

--- src/models/svZeroDModels/svZeroDModel.h
+++ src/models/svZeroDModels/svZeroDModel.h
@@ -1,5 +1,5 @@
-#ifndef CMLPNSVZEROD_H
-#define CMLPNSVZEROD_H
+#ifndef SVZERODMODEL_H
+#define SVZERODMODEL_H
 
 # include <string>
 # include <vector>
@@ -21,110 +21,53 @@
 
 using namespace std;
 
-// DEFINE CONVERSION CONSTANT
-const double pConv = 1.334E3;
-const double qConv = 1E0;
-
-class cmLPN_svZeroD: public cmModel {
-
-   
+class svZeroDModel {
   public:
 	//DEFAULT CONSTRUCTOR
-	cmLPN_svZeroD(std::string model_path);
+	svZeroDModel();
+	
+    //DEFAULT DESTRUCTOR
+	virtual ~svZeroDModel();
 
 	// GET NUMBER OF PARAMETERS
-	virtual int getParameterTotal();
+	virtual int getParameterTotal() = 0;
 
 	// GET NUMBER OF PARAMETERS (UKNOWNS)
-	virtual int getStateTotal();
+	virtual int getStateTotal() = 0;
 
 	// GET TOTAL NUMBER OF RESULTS
-	virtual int getResultTotal();
-  
-    // RETURN THE NUMBER OF EXTRA OUTPUTS
-    virtual int  getAuxStateTotal();
+	virtual int getResultTotal() = 0;
 
 	// GET THE PARAMETER NAMES
-	virtual string getParamName(int index);
+	virtual string getParamName(int index) = 0;
 
 	// GET PARAMETER SETS FOR THE LPN MODELS     
-	virtual void getDefaultParams(stdVec& zp);
+	virtual void getDefaultParams(stdVec& zp) = 0;
 
 	// GET THE PARAMETER RANGES FOR THE LPN MODELS
-	virtual void getParameterLimits(stdVec& limits);
+	virtual void getParameterLimits(stdVec& limits) = 0;
 
     virtual void getDefaultParameterLimits(stdVec& limits);
 
 	// SOLVE MODEL AND GET LOG LIKELIHOOD TO INTERFACE WITH DREAM
-	virtual double evalModelError(const stdVec& inputs, stdVec& outputs, stdIntVec& errorCode);
+	virtual double evalModelError(const stdVec& inputs, stdVec& outputs, stdIntVec& errorCode) = 0;
 
 	// GET RESULT NAME
-	string getResultName(int index);
+	virtual string getResultName(int index) = 0;
 
     // READ CORONARY PARAMETERS FROM FILE
-    virtual void readParamsFromFile(stdVec& inputs, std::string param_path);
+    virtual void readParamsFromFile(stdVec& inputs, std::string param_path) = 0;
 
     // PRINT OUT RESULTS
-    virtual void printResults(int totalResults, double *Xn);
+    virtual void printResults(int totalResults, double *Xn) = 0;
     
     // Read target data from file
-    virtual void readTargetsFromFile(string targetFileName);
+    virtual void readTargetsFromFile(string targetFileName) = 0;
 
-    virtual void getPriorMapping(int priorModelType,int* prPtr);
-
-  protected:
-
-        LPNSolverInterface interface;
-        int num_blocks;
-        std::vector<double> init_state_y, init_state_ydot;
-        std::vector<double> coronary_params;
-        std::vector<double> rcr_params;
-        std::vector<double> heart_params;
-        std::vector<double> vessel_params;
-        std::vector<int> Q_rcr_ids;
-        std::vector<int> Q_lca_ids;
-        std::vector<int> Q_rca_ids;
-        int Q_aorta_id = -1, P_aorta_id = -1, P_pul_id = -1, Q_LV_id = -1, Q_LA_id = -1, Q_RV_id = -1, V_LV_id = -1, P_RA_id = -1, P_RV_id = -1;
-
-  	int numCycles;
-  	int total3DSteps;
-  	int total0DSteps;
-        // Number of outlets and unknowns
-        int nUnknowns;
-        int nFaces;
-        int nCOR;
-        int nCOR_l;
-        int nCOR_r;
-        int nRCR;
-        // Base vectors to store raw output from reading mesh data
-        std::vector<double> Ra_l_base;
-        std::vector<double> Ra_r_base;
-        std::vector<double> Ram_l_base;
-        std::vector<double> Ram_r_base;
-        std::vector<double> Rv_l_base;
-        std::vector<double> Rv_r_base;
-        std::vector<double> Cim_l_base;
-        std::vector<double> Ca_l_base;
-        std::vector<double> Cim_r_base;
-        std::vector<double> Ca_r_base;
-        std::vector<double> Rp_rcr_base;
-        std::vector<double> Rd_rcr_base;
-        std::vector<double> C_rcr_base;
-        double Rrv_base;
-        double Rlv_base;
-        double Rpd_base;
-        // Flag which determines which surrogate model to run
-        bool useRigidSurrogate;
+    virtual void getPriorMapping(int priorModelType,int* prPtr) = 0;
         
-        // Target data needed for setting initial conditions
-        double targetESV;
-        double targetEDV;
-        double targetPaoMin;
-        double targetPaoMax;
+    int  solveCoronaryLPN(double* params, double* results) = 0;
 
-        // Assign parameter values from input
-        //void assignParameters(double* params);
-        int  solveCoronaryLPN(double* params, double* results);
 };
 
-#endif // CMLPNSVZEROD_H
+#endif // SVZERODMODEL_H
