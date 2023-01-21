b47fc61158cef7513d38b235e3ad6821455ff2da
Karthik Menon <karthikmenon.13@gmail.com>
2023-01-21 13:35:14 -0800

    svZeroDModel.h fn defs match svZeroD_ClosedLoopCoronary.h

--- src/models/svZeroDModels/svZeroDModel.h
+++ src/models/svZeroDModels/svZeroDModel.h
@@ -29,11 +29,17 @@ class svZeroDModel {
     // DESTRUCTOR
 	virtual ~svZeroDModel();
 
+    // SET UP MODEL PARAMETERS
+    virtual void setupModel(LPNSolverInterface& interface) = 0;
+
 	// GET NUMBER OF PARAMETERS
 	virtual int getParameterTotal() = 0;
 
 	// GET NUMBER OF PARAMETERS (UKNOWNS)
 	virtual int getStateTotal() = 0;
+  
+    // RETURN THE NUMBER OF EXTRA OUTPUTS
+    virtual int  getAuxStateTotal() = 0;
 
 	// GET TOTAL NUMBER OF RESULTS
 	virtual int getResultTotal() = 0;
@@ -41,36 +47,33 @@ class svZeroDModel {
 	// GET THE PARAMETER NAMES
 	virtual string getParamName(int index) = 0;
 
+	// GET RESULT NAME
+	virtual string getResultName(int index) = 0;
+
 	// GET PARAMETER SETS FOR THE LPN MODELS     
 	virtual void getDefaultParams(stdVec& zp) = 0;
 
 	// GET THE PARAMETER RANGES FOR THE LPN MODELS
 	virtual void getParameterLimits(stdVec& limits) = 0;
 
-    virtual void getDefaultParameterLimits(stdVec& limits);
-
-	// SOLVE MODEL AND GET LOG LIKELIHOOD TO INTERFACE WITH DREAM
-	virtual double evalModelError(const stdVec& inputs, stdVec& outputs, stdIntVec& errorCode) = 0;
-
-	// GET RESULT NAME
-	virtual string getResultName(int index) = 0;
-
-    // READ CORONARY PARAMETERS FROM FILE
-    virtual void readParamsFromFile(stdVec& inputs, std::string param_path) = 0;
-
-    // PRINT OUT RESULTS
-    virtual void printResults(int totalResults, double *Xn) = 0;
-    
-    // Read target data from file
-    virtual void readTargetsFromFile(string targetFileName) = 0;
+    virtual void getDefaultParameterLimits(stdVec& limits) = 0;
 
     virtual void getPriorMapping(int priorModelType,int* prPtr) = 0;
         
+    // UPDATE ZEROD MODEL PARAMETERS
     virtual void setModelParams(LPNSolverInterface& interface) = 0;
 
-    virtual void setupModel(LPNSolverInterface& interface) = 0;
+    // POSTPROCESS ZEROD SIMULATION
+    virtual void postProcess(LPNSolverInterface& interface, const stdMat& outVals,const stdMat& auxOutVals, stdVec& results) = 0;
 
-    virtual void postProcess(double timeStep, int totalStepsOnSingleCycle, int totalSteps, const stdVec& params, const stdMat& outVals,const stdMat& auxOutVals, stdVec& results) = 0;
+    // KEY/NAME FOR EACH TARGET QUANTITY
+    virtual void getResultKeys(stdVec& keys) = 0;
+
+    // STANDARD DEVIATION OF EACH TARGET MEASUREMENT
+    virtual void getDataStd(stdVec& stdFactors) = 0;
+
+    // INVERSE WEIGHT OF EACH TARGET QUANTITY IN LOG LIKELIHOOD
+    virtual void getResultWeights(stdVec& weights) = 0;
 
 };
 
