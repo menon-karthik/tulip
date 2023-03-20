31747cf3054f91ee330847444ab7bd882cbc16e8
Karthik Menon <kmenon13@sh03-ln05.stanford.edu>
2023-03-20 14:13:46 -0700

    command line arg for svzerod json

--- test/cppTests/07_optSvZeroD/test_optSvZeroD.cpp
+++ test/cppTests/07_optSvZeroD/test_optSvZeroD.cpp
@@ -10,63 +10,81 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-
-  double ll = 0.0;
+  
+  // Parse command-line inputs
+  std::string model_path;
+  if(argc < 1 || argc > 2){
+    printf("Usage: optSvZeroD (optional: model_path)\n");
+    printf("Terminated!\n");
+    exit(1);
+  }else{
+    try{
+      // Get svZeroD model json file
+      if(argc == 2) {
+        model_path = string(argv[1]);
+      } else {
+        model_path = "svzerod_tuning.json"; // Default
+      }
+    }catch(exception &e){
+      // ERROR: TERMINATED!
+      printf("Invalid Input Arguments\n");
+      printf("TERMINATED!\n");
+      return 0;
+    }
+  }
     
-  try{
+  // Create new data object
+  int keyColumn = 0;
+  int timeStampColumn = 0;
+  int columnID = 1; // N-1 after column with keys
+  bool useSingleColumn = true;
+  string currPatientFile("coronary.csv");
+  daData* data = new daData_multiple_Table(useSingleColumn,columnID);
+  data->readFromFile(currPatientFile);
 
-    // Create new data object
-    int keyColumn = 0;
-    int timeStampColumn = 0;
-    int columnID = 1; // N-1 after column with keys
-    bool useSingleColumn = true;
-    string currPatientFile("coronary.csv");
-    daData* data = new daData_multiple_Table(useSingleColumn,columnID);
-    data->readFromFile(currPatientFile);
+  // Create new LPN model
+  // svZeroDPlus interface library
+  auto interface_lib = std::string("/home/users/kmenon13/svZeroDPlus/Release-master/src/interface/libsvzero_interface_library.so");
+  // Type of svZeroD model
+  svZeroDModel* zeroDmodel = new svZeroD_ClosedLoopCoronary();
+  cmLPN_svZeroD* lpnModel;
+  lpnModel = new cmLPN_svZeroD(model_path, zeroDmodel, interface_lib);
 
-    // Create new LPN model
-    // svZeroDPlus interface library
-    auto interface_lib = std::string("/home/users/kmenon13/svZeroDPlus/Release-master/src/interface/libsvzero_interface_library.so");
-    // svZeroD json file
-    std::string model_path = "svzerod_tuning.json";
-    // Type of svZeroD model
-    svZeroDModel* zeroDmodel = new svZeroD_ClosedLoopCoronary();
-    cmLPN_svZeroD* lpnModel;
-    lpnModel = new cmLPN_svZeroD(model_path, zeroDmodel, interface_lib);
+  // Assign Dataset to model
+  int currPatient = 0;
+  lpnModel->setData(data);
 
-    // Assign Dataset to model
-    int currPatient = 0;
-    lpnModel->setData(data);
+  // Set Optimizer Parameters
+  // Total Number of iterations
+  int totIterations = 50;
+  // Convergence Tolerance
+  double convTol = 1.0e-1;
+  // Check Convergence every convUpdateIt iterations
+  int convUpdateIt = 10;
+  // Maximum Iterations
+  int maxOptIt = 200;
+  // Coefficient for Step increments
+  double stepCoefficient = 0.1;
+  // File with initial starting point
+  bool useStartingParameterFromFile = true;
+  bool startFromCentre = false;
+  string startParameterFile("coronaryParams.txt");
 
-    // Set Optimizer Parameters
-    // Total Number of iterations
-    int totIterations = 50;
-    // Convergence Tolerance
-    double convTol = 1.0e-1;
-    // Check Convergence every convUpdateIt iterations
-    int convUpdateIt = 10;
-    // Maximum Iterations
-    int maxOptIt = 200;
-    // Coefficient for Step increments
-    double stepCoefficient = 0.1;
-    // File with initial starting point
-    bool useStartingParameterFromFile = true;
-    bool startFromCentre = false;
-    string startParameterFile("coronaryParams.txt");
+  // Construct Action
+  acAction* nm = new acActionOPT_NM(convTol, 
+                                    convUpdateIt,
+                                    maxOptIt,
+                                    stepCoefficient);
+  
+  // Set Model
+  nm->setModel(lpnModel);
 
-    // Construct Action
-    acAction* nm = new acActionOPT_NM(convTol, 
-                                      convUpdateIt,
-                                      maxOptIt,
-                                      stepCoefficient);
-    
-    // Set Model
-    nm->setModel(lpnModel);
-
-    // SET INITIAL GUESS
-    ((acActionOPT_NM*)nm)->setInitialParamGuess(useStartingParameterFromFile,
+  // SET INITIAL GUESS
+  ((acActionOPT_NM*)nm)->setInitialParamGuess(useStartingParameterFromFile,
                                                 startFromCentre,
                                                 startParameterFile);        
+
+  try{
 
     for(int loopA=0;loopA<totIterations;loopA++){
       
@@ -87,8 +105,6 @@ int main(int argc, char* argv[]){
   }
 
   // COMPLETED
-  printf("\n");
-  printf("Obj: %f\n",ll);
   printf("COMPLETED!\n");
   return 0;
 }
